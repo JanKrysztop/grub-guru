37fc7bf9a93e4ef4a53738c617652e4116e1a6ff
Jan Krysztop <jan.krysztop@dimark.eu>
2024-02-24 16:19:38 +0100

    add new navigation menu to main-header

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -13,7 +13,20 @@ import { useColorScheme } from "@mui/joy/styles";
 import DarkMode from "@mui/icons-material/DarkMode";
 import LightMode from "@mui/icons-material/LightMode";
 import IconButton from "@mui/joy/IconButton";
+import MenuButton from "@mui/joy/MenuButton";
+import MenuItem from "@mui/joy/MenuItem";
+import Dropdown from "@mui/joy/Dropdown";
+import Menu from "@mui/joy/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import LocalDining from "@mui/icons-material/LocalDining";
+import Calculate from "@mui/icons-material/Calculate";
+import NoteAlt from "@mui/icons-material/NoteAlt";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Logout from "@mui/icons-material/Logout";
+import ListItemDecorator from "@mui/joy/ListItemDecorator";
+import ListDivider from "@mui/joy/ListDivider";
 import { Container, Box } from "@mui/joy";
+import { Note } from "@mui/icons-material";
 
 //TODO: create custom 404 page
 const MainHeader = () => {
@@ -50,24 +63,91 @@ const MainHeader = () => {
           p: 3,
         }}
       >
-        <IconButton
-          variant="soft"
-          sx={{
-            borderRadius: "40px",
-            backgroundColor: mode === "dark" ? "#292b29" : "#fafafa", // Replace '#yourLightModeColor' with the desired color for light mode
-            transition: "transform 0.3s ease-in-out", // Smooth transition for the hover effect
-            "&:hover": {
-              transform: "scale(1.1)", // Slightly increase the size on hover
-              backgroundColor:
-                mode === "dark"
-                  ? "#hoverColorDarkMode"
-                  : "#hoverColorLightMode", // Optionally change the background color on hover for each mode
-            },
-          }}
-          onClick={() => setMode(mode === "dark" ? "light" : "dark")}
-        >
-          {mode === "dark" ? <DarkMode /> : <LightMode />}
-        </IconButton>
+        <Box>
+          <Dropdown>
+            <MenuButton
+              slots={{ root: IconButton }}
+              slotProps={{ root: { variant: "plain", color: "neutral" } }}
+              sx={{ borderRadius: 40 }}
+            >
+              <MenuIcon />
+            </MenuButton>
+            <Menu
+              placement="bottom-start"
+              variant="soft"
+              sx={{
+                borderRadius: "8px",
+                boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+                "--List-padding": "0.5rem",
+                "--ListItemDecorator-size": "3rem",
+                // gap: 1,
+              }}
+            >
+              <Link href="/calories-tracker">
+                <MenuItem>
+                  <ListItemDecorator>
+                    <LocalDining />
+                  </ListItemDecorator>{" "}
+                  Calories tracker
+                </MenuItem>
+              </Link>
+              <ListDivider />
+              <Link href="/calculator">
+                <MenuItem>
+                  <ListItemDecorator>
+                    <Calculate />
+                  </ListItemDecorator>{" "}
+                  Calculators
+                </MenuItem>
+              </Link>
+              <ListDivider />
+              <Link href="/journal">
+                <MenuItem>
+                  <ListItemDecorator>
+                    <NoteAlt />
+                  </ListItemDecorator>{" "}
+                  Journal
+                </MenuItem>
+              </Link>
+              <ListDivider />
+
+              <Link href="/profile">
+                <MenuItem>
+                  <ListItemDecorator>
+                    <AccountCircle />
+                  </ListItemDecorator>{" "}
+                  Profile
+                </MenuItem>
+              </Link>
+              <ListDivider />
+              <MenuItem onClick={handleLogout}>
+                <ListItemDecorator>
+                  <Logout />
+                </ListItemDecorator>{" "}
+                Logout
+              </MenuItem>
+            </Menu>
+          </Dropdown>
+
+          <IconButton
+            variant="soft"
+            sx={{
+              borderRadius: "40px",
+              backgroundColor: mode === "dark" ? "#292b29" : "#fafafa", // Replace '#yourLightModeColor' with the desired color for light mode
+              transition: "transform 0.3s ease-in-out", // Smooth transition for the hover effect
+              "&:hover": {
+                transform: "scale(1.1)", // Slightly increase the size on hover
+                backgroundColor:
+                  mode === "dark"
+                    ? "#hoverColorDarkMode"
+                    : "#hoverColorLightMode", // Optionally change the background color on hover for each mode
+              },
+            }}
+            onClick={() => setMode(mode === "dark" ? "light" : "dark")}
+          >
+            {mode === "dark" ? <DarkMode /> : <LightMode />}
+          </IconButton>
+        </Box>
         <img src="/big-logo.svg" alt="Big Logo" style={{ width: "20%" }} />
         <nav>
           <ul className="flex space-x-4">
