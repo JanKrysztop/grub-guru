1be29ed508eeaae26cbc20ee7b48194938d97f0c
Jan Krysztop <jan.krysztop@dimark.eu>
2024-03-19 10:13:09 +0100

    add AppLayout component

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { CssVarsProvider } from "@mui/joy/styles";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "@/redux/store";
-import Layout from "@/components/layout/Layout";
+import Layout from "@/components/layout/AppLayout";
 
 const App = ({ Component, pageProps }) => {
   return (
